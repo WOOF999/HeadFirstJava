ffee4ab863b29510f97135d902e0e3b99ba67dd3
WOOF999 <eunha092559@gmail.com>
2022-08-21 01:08:53 +0900

    ch12 commit

---
+++ src/CH12/SimpleGui3.java
@@ -0,0 +1,11 @@
+package CH12;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SimpleGui3 {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+
+}
